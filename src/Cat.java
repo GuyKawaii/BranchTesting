5953765411525ac79dea9a4dec9689e482462757
Daniel Alexander Dam <dani36i9@stud.dk>
2022-04-05 14:16:08 +0200

    Add override change

--- src/Cat.java
+++ src/Cat.java
@@ -1,4 +1,6 @@
 public class Cat extends Animal{
+  
+  @Override
   public void makeSound() {
     System.out.println("Miao!");
   }
--- src/Dog.java
+++ src/Dog.java
@@ -1,4 +1,6 @@
 public class Dog extends Animal {
+  
+  @Override
   public void makeSound() {
     System.out.println("Woof Woof!");
   }
